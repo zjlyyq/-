abde66a16a2acaf08906251e2d212891d9b6552e
jialuzhang <jialuzhang0805@gmail.com>
2018-12-08 09:39:54 +0800

    init

---
+++ Lesson01_ES6/es5.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Created by jialuzhang on 2018/12/7.
+ */
+var name = 'zjl';
+console.log(name);
+var results = [1, 2, 3].map(function (x) {
+  return x + 2;
+});
+console.log(results);
---
+++ Lesson01_ES6/es6.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jialuzhang on 2018/12/7.
+ */
+let name = 'zjl';
+// console.log(name);
+let results = [1,2,3].map((x)=>{
+    return x + 2;
+})
+console.log(results);
+document.body.innerHTML = 'Hello World';
+
+{
+    let fruit = 'apple';
+    var food = 'rice';
+}
+
+console.log(food);
